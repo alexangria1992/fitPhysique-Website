408a4db0e623bf9306c26231fcad1f79f9a5532c
Alexandre Angrignon <alex040892@gmail.com>
2024-06-03 15:05:36 +1200

    Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import HeroSlider from './HeroSlider';
 const Hero = () => {
-  return <section id='home' className={styles.heroContainer}></section>;
+  return (
+    <section id='home' className={`${styles.heroSection} bg-hero `}>
+      <div className={styles.heroContainer}>
+        {/* Slider */}
+        <HeroSlider />
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
