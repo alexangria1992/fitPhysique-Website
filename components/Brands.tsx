7fc7d767690302f819e5bf61273b0d711dc7c962
Alexandre Angrignon <alex040892@gmail.com>
2024-06-05 14:11:45 +1200

    brands section

--- components/Brands.tsx
+++ components/Brands.tsx
@@ -1,9 +1,91 @@
-import React from 'react';
+'use client';
+
 import styles from './Brands.module.css';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+const brandImages = [
+  {
+    src: '/assets/img/brands/brand-1.png',
+    href: '',
+  },
+  {
+    src: '/assets/img/brands/brand-2.png',
+    href: '',
+  },
+  {
+    src: '/assets/img/brands/brand-3.png',
+    href: '',
+  },
+  {
+    src: '/assets/img/brands/brand-4.png',
+    href: '',
+  },
+  {
+    src: '/assets/img/brands/brand-5.png',
+    href: '',
+  },
+];
+
+import { motion } from 'framer-motion';
+
+// variants
+const brandContainerVariant = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.4,
+      duration: 0.5,
+      ease: 'linear',
+    },
+  },
+};
+
+const brandItem = {
+  hidden: {
+    y: 20,
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: [0.25, 0.6, 0.3, 0.8],
+    },
+  },
+};
 const Brands = () => {
   return (
     <section id='contact' className={styles.brandsContainer}>
-      Brands
+      <div className='container mx-auto'>
+        <motion.div
+          variants={brandContainerVariant}
+          initial='hidden'
+          whileInView={'show'}
+          viewport={{ once: false, amount: 0.3 }}
+          className={styles.brandsWrapper}
+        >
+          {brandImages.map((img, index) => {
+            return (
+              <motion.div variants={brandItem} key={index}>
+                <Link className={`${styles.blogLink} group`} href={img.href}>
+                  <Image
+                    className={styles.brandsImage}
+                    src={img.src}
+                    width={204}
+                    height={106}
+                    alt=''
+                  />
+                </Link>
+              </motion.div>
+            );
+          })}
+        </motion.div>
+      </div>
     </section>
   );
 };
